223ddbae0578f6c0c67e84871b2364bc88d849e4
Umma Salma <salma.stu2017@juniv.edu>
2022-01-09 21:42:10 +0600

    Updated urls.py

--- OnlineClassAndExam/base/urls.py
+++ OnlineClassAndExam/base/urls.py
@@ -9,10 +9,10 @@ urlpatterns= [
 
     path('room/<str:pk>/', views.room, name="room"),
 
-    path('create-room/', views.createRoomSingleton, name="create-room"),
-    path('update-room/<str:pk>/', views.updateRoomSingleton, name="update-room"),
-    path('delete-room/<str:pk>/', views.deleteRoomSingleton, name="delete-room"),
-    path('delete-message/<str:pk>/', views.deleteMessageSingleton, name="delete-message"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
 
     path('activity/', views.activityPageSingleton, name="activity"),
 ]

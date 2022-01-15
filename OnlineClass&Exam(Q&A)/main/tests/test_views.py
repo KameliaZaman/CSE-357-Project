9b1e63daffef17e7f7e2b84ef61a15f59bbd40b6
Umma Salma <salma.stu2017@juniv.edu>
2022-01-15 18:55:43 +0600

    Updated test_views.py

--- OnlineClass&Exam(Q&A)/main/tests/test_views.py
+++ OnlineClass&Exam(Q&A)/main/tests/test_views.py
@@ -1,5 +1,3 @@
-
-
 from pydoc import cli
 from unittest import TestCase
 from django.test import TestCase, Client
@@ -10,20 +8,32 @@ import json
 
 
 class testViews(TestCase):
+		"""
+		This class tests views.py.
+		"""
 
 		def setUp(self):
-				self.client = Client()
-				self.registerUrl = reverse('register')
-				self.loginUrl = reverse('login')
-				# self.logoutUrl = reverse('logout')
+			"""
+			This function sets up the client, registerUrl and loginUrl.
+			"""
+			self.client = Client()
+			self.registerUrl = reverse('register')
+			self.loginUrl = reverse('login')
+			# self.logoutUrl = reverse('logout')
 
 		def testRegisterPagePOST(self):
+				"""
+				This function tests register page.
+				"""
 				response = self.client.get(self.registerUrl)
 
 				self.assertEquals(response.status_code, 200)
 				self.assertTemplateUsed(response, 'register.html')
 
 		def testLoginPagePOST(self):
+				"""
+				This function tests login page.
+				"""
 				response = self.client.get(self.loginUrl)
 
 				self.assertEquals(response.status_code, 200)

afc2beeec44f0e34b5a0c5704610070b45214d06
Kamelia Zaman Moon <44264891+KameliaZaman@users.noreply.github.com>
2022-01-15 23:40:30 +0600

    Revert "Code check"

--- media/codeFile/Factorial_uf6YL0V.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter an integer:"))
-fact = 1
-for factor in range(n, 1, -1):
-    fact = fact * factor
-print("The Factor is: ", fact)
--- media/codeFile/Practice_09TD67g.py
+++
@@ -1,8 +0,0 @@
-cars = 20
-people = 30
-if cars > people:
-    print("Cars")
-elif cars < people:
-    print("people")
-else:
-    print("We can't decide")

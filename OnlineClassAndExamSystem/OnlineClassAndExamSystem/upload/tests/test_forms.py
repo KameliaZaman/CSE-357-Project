196350d3146683cfeaedc8145f2946bd45e5460e
Tasmim Sultana <tasmimsinthia@gmail.com>
2022-01-29 19:32:37 +0600

    Modified test file

--- OnlineClassAndExamSystem/OnlineClassAndExamSystem/upload/tests/test_forms.py
+++ OnlineClassAndExamSystem/OnlineClassAndExamSystem/upload/tests/test_forms.py
@@ -2,16 +2,6 @@ from django.test import SimpleTestCase
 from upload.forms import questionAndSolutionForm
 
 class TestForms(SimpleTestCase):
-    def test_quses_solution_form_valid_data(self):
-        form = questionAndSolutionForm(data={
-            "subject" : "Operating System",
-            "semester" : "Third Year First",
-            "question" : "",
-            "solution" : "dspAnswer_2HJSy4N.pdf"
-
-        })
-
-        self.assertTrue(form.is_valid())
 
     def test_ques_solution_form_no_data(self):
         form = questionAndSolutionForm(data={})
